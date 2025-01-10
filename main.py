861573c429df9ce128413fc608653c4d45789c07
salataikttss <sibgatovy@gmail.com>
2025-01-10 11:28:44 +0300

    file main.py
    add main.py file (program)

---
+++ main.py
@@ -0,0 +1,9 @@
+def filter_short_strings(array):
+    return [string for string in array if len(string) <= 3]
+
+def main():
+    input_array = ["Hello", "2", "world", ":-)" ]  # Example input
+    result = filter_short_strings(input_array)
+    print(result)
+
+main()
